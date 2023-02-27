57801320861c3471a4fd8b215e3108ec45f8e819
LuisDometic <luis.kobayashi@dometic.com>
2023-02-27 15:46:09 +0100

    Local button DDM test

--- src/components/atoms/Buttons/LocalButton/presets/0-default.jsx
+++ src/components/atoms/Buttons/LocalButton/presets/0-default.jsx
@@ -3,5 +3,6 @@ import LocalButton from "../LocalButton";
 export default (
   <LocalButton
     uxpId="local-button-1"
-    text="Click Me!"/>
+    text="Click Me!"
+    ddmParam="Add DDM here"/>
 );
