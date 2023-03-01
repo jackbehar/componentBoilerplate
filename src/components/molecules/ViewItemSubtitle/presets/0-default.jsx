34ee6f165fab8e545cb40468a4fc879801a338fa
LuisDometic <luis.kobayashi@dometic.com>
2023-03-01 10:10:00 +0100

    ViewItem props test

--- src/components/molecules/ViewItemSubtitle/presets/0-default.jsx
+++ src/components/molecules/ViewItemSubtitle/presets/0-default.jsx
@@ -1,5 +1,16 @@
 import ViewItemSubtitle from "../ViewItemSubtitle";
+import ColdBigIcon from "../../../../assets/icons/SVG/ColdBigIcon";
+import i18n from "../../../../localization/config/i18n";
+import translations from "../../../../localization/config/translationHelper";
+import {IconProps} from '../../../../types/icons';
 
 export default (
-  <ViewItemSubtitle uxpId="viewItem-1" name="Title" subtitle="Subtitle" />
+  <ViewItemSubtitle
+    uxpId="viewItem-1"
+    displayValue={i18n.t(translations.landing.common.manage)}
+    name={"Item Name"}
+    subtitle={"Subtitle"}
+    onPress={() => console.log(`viewItemSubtitle PRESSED!!`)}
+    Icon={ColdBigIcon}
+  />
 );
