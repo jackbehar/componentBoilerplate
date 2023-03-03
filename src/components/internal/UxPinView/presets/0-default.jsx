dbd4128c6516557939240d9f495fa54c0ea4ed15
Markuson <el.marcuson@gmail.com>
2023-03-03 09:51:11 +0100

    uxPinview trying to show styles

--- src/components/internal/UxPinView/presets/0-default.jsx
+++ src/components/internal/UxPinView/presets/0-default.jsx
@@ -1,5 +1,10 @@
 import UxPinView from "../index";
 import React from "react";
+import * as PropTypes from "prop-types";
+
+UxPinView.propTypes = {
+  styles: PropTypes.object,
+};
 
 export default (
   <UxPinView uxpId="UxPinView-1"/>
